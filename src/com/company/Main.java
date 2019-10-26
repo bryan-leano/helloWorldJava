b9fbfc94daca6004507d5263b6222ac1f3f872b1
Bryan Leanoe <bryan.leano@gmail.com>
2019-10-26 00:07:09 -0600

    add greet name

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -3,6 +3,11 @@ package com.company;
 public class Main {
 
     public static void main(String[] args) {
+        greet("Bryan");
+    }
+
+    public static void greet(String name) {
         System.out.println("Hello World!");
+        System.out.println("Hello, " + name + ". How are you doing today?");
     }
 }
